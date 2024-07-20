e80939be1ff248eda2f31e65c3cf9abfd76d48e7
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-07-20 20:48:25 +0500

    Updated sendVerificationemail

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -8,6 +8,12 @@ export async function sendVerificationEmail(
     verifyCode: string
 ): Promise<ApiResponse> {
     try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: ['delivered@resend.dev'],
+            subject: 'Hello world',
+            react: EmailTemplate({ firstName: 'John' }),
+          });
         return {success: true, message:"Verification email send successfully"}
     }catch(emailError){
         console.error("Error sending verification email", emailError)
