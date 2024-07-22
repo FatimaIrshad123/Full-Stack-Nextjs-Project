5a52e3f5e286338aef009472864f86de4ec9897b
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-07-22 20:22:57 +0500

    Added sign-up routes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,12 +2,64 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from 'bcryptjs';
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { string } from "zod";
 
 export async function POST(request: Request) {
     await dbConnect()
-
+    
     try {
         const {username,email, password} = await request.json()
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+        if (existingUserVerifiedByUsername){
+            return Response.json({
+                success: false,
+                message: 'Username is already taken'
+            },{status: 400})
+        }
+        const existingUserByEmail = await UserModel.findOne({email})
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+        if (existingUserByEmail){
+            true
+        }else {
+            const hashedPassword = await bcrypt.hash(password,10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() +1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+            await newUser.save();
+        }
+
+        // send verification email
+        const emailRespnse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        )
+
+        if (!emailRespnse.success){
+            return Response.json({
+                success: false,
+                message: emailRespnse.message
+            }, {status: 500})
+        }
+
+        return Response.json({
+            success: true,
+            message: "User registered successfully please verify your email"
+        }, {status: 500})
+        
     }catch(error){
         console.log('Error registering user', error)
         return Response.json({
