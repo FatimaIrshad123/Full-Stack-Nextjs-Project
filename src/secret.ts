9ba8bbfb93ea03d7b15826c7c19dcaa416407228
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-07-30 23:08:30 +0500

    Added unique-username routes

--- src/secret.ts
+++ src/secret.ts
@@ -1,2 +1,2 @@
-export const MONGODB_URL = 'mongodb+srv://Fatima:Fatima@cluster0.7bunznc.mongodb.net/Nextjs-project'
-export const RESEND_API_KEY = 're_Cqjdtyh3_54M4JsveEE5JSmjNxK6LhT78'
+export const MONGODB_URL = ''
+export const RESEND_API_KEY = ''
