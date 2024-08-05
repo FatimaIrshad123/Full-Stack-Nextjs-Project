f257863d8e40cc5bab45834ba17cdaa05e021677
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-08-05 22:56:16 +0500

    starting frontend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -76,7 +76,16 @@ const page = () => {
     }
 
   return (
-    <div>Page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 rounded-lg shadow-md bg-white space-y-8">
+        <div className="text-center">
+          <h1 className="text-4xl tracking-tight lg:text-5xl mb-6 font-extrabold">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign up to start your anonymus adventure </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
