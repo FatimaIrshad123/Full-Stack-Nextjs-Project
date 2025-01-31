a2f797ca404e5c2dcdb38ab3d7f661ec222b9fe5
FatimaIrshad123 <bibimemoona2017@gmail.com>
2025-01-31 22:31:55 +0500

    Updated styling

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
   
   return (
     <>
-    <main className="flex flex-grow flex-col items-center justify-center px-4 py-12 md:px-24">
-      <section className="text-center mb-8 md:mb-12">
+    <main className="flex flex-grow flex-col items-center justify-center px-4 py-12 md:px-24 rounded-md m-2 bg-cyan-700">
+      <section className="text-center mb-8 md:mb-12 text-white">
         <h1 className="text-3xl md:text-5xl font-bold">Dive into the world of Anonymus</h1>
         <p className="mt-3 md:mt-4 text-base md:text-lg">Explore mystery message - Where your identity remain a secret</p>
         <p>Click to visit <Link href='/dashboard'>Dashboard</Link></p>
@@ -31,7 +31,7 @@ export default function Home() {
           messages.map((message,index) => (
               <CarouselItem key={index}>
               <div className="p-1">
-                <Card>
+                <Card className="text-white bg-cyan-300">
                   <CardHeader>
                     {message.title}
                   </CardHeader>
@@ -48,7 +48,7 @@ export default function Home() {
       <CarouselNext />
     </Carousel>
     </main>
-    <footer className="text-center p-4 md:p-6">2023 Mystery Message. All rights reserved</footer>
+    <footer className="text-center p-4 md:p-6 bg-cyan-500 m-2 rounded-md text-white">2025 Mystery Message. All rights reserved</footer>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,11 @@ import { Button } from "./ui/button"
 
 
 const Navbar = () => {
-
     const {data: session} = useSession()
-
     const user: User = session?.user as User
 
     return (
-        <nav className="p-4 md:p-6 shadow-md">
+        <nav className="p-4 md:p-6 shadow-md bg-cyan-500 rounded-md m-2 text-white">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
                 <a className="text-xl font-bold mb-4 md:mb-0" href="#">Mystry Message</a>
                 {
